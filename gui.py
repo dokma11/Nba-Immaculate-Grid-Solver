2ec8f84961c0ebaa66521e314369460badd1796d
dokma11 <vule.dok@gmail.com>
2024-02-07 20:12:49 +0100

    feat: GUI Implementation

---
+++ gui.py
@@ -0,0 +1,45 @@
+import customtkinter
+from tkinter import messagebox
+
+
+class Gui:
+    def __init__(self):
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("dark-blue")
+
+        self.root = customtkinter.CTk()
+        self.root.title("Immaculate Grid Solver")
+        self.root.geometry("500x350")
+
+        self.frame = customtkinter.CTkFrame(master=self.root)
+        self.frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+        self.label = customtkinter.CTkLabel(master=self.frame, text="Immaculate grid solver", font=("Roboto", 24))
+        self.label.pack(pady=12, padx=10)
+
+        self.label = customtkinter.CTkLabel(master=self.frame, text="Enter grid URL", font=("Roboto", 18))
+        self.label.pack(pady=12, padx=10)
+
+        self.input1 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Grid URL")
+        self.input1.pack(pady=12, padx=10)
+
+        self.button = customtkinter.CTkButton(master=self.frame, text="Submit", command=self.solve)
+        self.button.pack(pady=12, padx=10)
+
+        self.return_value = ""
+
+    def solve(self):
+        print('URL provided: ' + self.input1.get())
+        if self.validate():
+            self.return_value = self.input1.get()
+            messagebox.showinfo('Immaculate Grid Solver', 'The application will now start searching for the right '
+                                                          'players!')
+            self.root.destroy()
+        else:
+            messagebox.showinfo('Alert', 'The provided URL is in incorrect format!')
+
+    def validate(self):
+        if "https://www.immaculategrid.com/basketball/mens/grid" in self.input1.get():
+            return True
+        else:
+            return False
