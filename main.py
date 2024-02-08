8b57ec16f28a3d544b75c4b97774a193dfe1e1fb
dokma11 <vule.dok@gmail.com>
2024-02-08 17:15:34 +0100

    feat: Implemented stats inclusion in search

--- main.py
+++ main.py
@@ -23,11 +23,27 @@ print(aria_labels)
 
 ps = PlayerSearch()
 
-first_team_list, second_team_list = ps.format_teams(aria_labels)
+first_team_list, second_team_list, only_teams = ps.format_teams(aria_labels)
 
 for i in range(len(first_team_list)):
-    response = ps.get_page(first_team_list[i], second_team_list[i])
-    ps.filter_players(ps.get_players_from_headers(ps.get_needed_headers(response)))
+    if only_teams[i]:
+        response = ps.get_page(first_team_list[i], second_team_list[i])
+        ps.filter_players(ps.get_players_from_headers(ps.get_needed_headers(response)))
+    else:
+        if '+' in first_team_list[i]:
+            first_team_list[i] = first_team_list[i].replace('+', '%2B')
+        first_team = first_team_list[i].replace(' ', '+')
+
+        if '+' in second_team_list[i]:
+            second_team_list[i] = second_team_list[i].replace('+', '%2B')
+        second_team = second_team_list[i].replace(' ', '+')
+
+        response = ps.get_page_with_stats(first_team, second_team)
+
+        response.raise_for_status()
+        print(response.status_code)
+
+        ps.add_players_with_stats(ps.get_needed_headers_with_stats(response))
 
 for player in ps.players:
     print(player)
